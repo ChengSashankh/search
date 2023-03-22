f8abbee9c12f51db60ef7b0eee258ba935b7f45f
Sashankh Chengavalli Kumar <cksashankh@gmail.com>
2023-03-23 00:32:36 +0800

    Add basic inverted indexer

---
+++ backend/extractor/src/main/scala/fyi/deeno/data/Page.scala
@@ -0,0 +1,8 @@
+package fyi.deeno.data
+
+case class Page(id: Long, title: String, text: String)
+case class Posting(word: String, id: Long, count: Long)
+
+case class TermFrequency(id: Long, freq: Long)
+case class TfInvertedPosting(word: String, postings: List[TermFrequency])
+case class InvertedPosting(word: String, docs: String)
---
+++ backend/extractor/src/main/scala/fyi/deeno/indexers/SimpleInvertedIndexer.scala
@@ -0,0 +1,24 @@
+package fyi.deeno.indexers
+
+import fyi.deeno.data.{InvertedPosting, Page, Posting}
+import org.apache.spark.sql.{Column, Dataset, SparkSession}
+import org.apache.spark.sql.functions.{col, collect_set, concat_ws, lit, concat}
+
+class SimpleInvertedIndexer(spark: SparkSession) {
+  def stringify(c: Column) = concat(lit("["), concat_ws(",", c), lit("]"))
+
+  def index(pageDf: Dataset[Page]): Dataset[InvertedPosting] = {
+    import spark.implicits._
+
+    pageDf
+      .flatMap(page => page.text.split(" ")
+              .map(word => word.trim.replaceAll("[^A-Za-z]+", ""))
+              .map(_.toLowerCase).filter(_.nonEmpty).filter(_.length < 28)
+              .map(word => Posting(word, page.id, 1))
+      )
+      .groupBy(col("word")).agg((collect_set("id")).alias("docs"))
+      .withColumn("docs", stringify($"docs"))
+      .as[InvertedPosting]
+  }
+
+}
