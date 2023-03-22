5ff07b709a2ed507c178df287f9c0c9cdf65dbb3
Sashankh Chengavalli Kumar <cksashankh@gmail.com>
2023-03-23 00:32:36 +0800

    Add basic inverted indexer

---
+++ backend/extractor/src/main/scala/fyi/deeno/readers/XMLDatasetReader.scala
@@ -0,0 +1,19 @@
+package fyi.deeno.readers
+
+import fyi.deeno.data.Page
+import org.apache.spark.sql.functions.col
+import org.apache.spark.sql.{DataFrame, Dataset, SparkSession}
+
+class XMLDatasetReader(spark: SparkSession) {
+
+  def read(sourceFilePath: String, rowTag: String = "page"): Dataset[Page] = {
+    import spark.implicits._
+
+    spark.read.format("com.databricks.spark.xml")
+      .option("rowTag", rowTag)
+      .load(sourceFilePath)
+      .select(col("id"), col("title"), col("revision.text._VALUE").alias("text"))
+      .as[Page]
+  }
+
+}
