ec1f6044272495cfa30d38a6e9a9652eaa768d1f
Sashankh Chengavalli Kumar <cksashankh@gmail.com>
2023-03-26 00:46:24 +0800

    Attempt to fix cors issue

---
+++ api/search-service/src/main/java/fyi/deeno/searchservice/auth/CorsFilter.java
@@ -0,0 +1,24 @@
+package fyi.deeno.searchservice.auth;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+public class CorsFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+        HttpServletRequest httpServletRequest = (HttpServletRequest) servletRequest;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Auth-Token, Content-Type, Accept, X-Requested-With, remember-me");
+
+        filterChain.doFilter(servletRequest, servletResponse);
+    }
+}
