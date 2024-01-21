9b04e5fe80d2aa3b6c188e5848c6a7c1340df30c
Sashankh Chengavalli Kumar <cksashankh@gmail.com>
2024-01-21 18:53:28 -0500

    WIP - previous session changes

--- api/search-service/src/main/java/fyi/deeno/searchservice/SearchServiceApplication.java
+++ api/search-service/src/main/java/fyi/deeno/searchservice/SearchServiceApplication.java
@@ -1,7 +1,9 @@
 package fyi.deeno.searchservice;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
